687d24e0528e365a820b4a9006f47b65e58541d2
Kareem Hamed <kareemhamed818@gmail.com>
2023-05-06 15:56:39 +0300

    Edit4 by kareem

--- lib/modules/company/offers/offers_screen.dart
+++ lib/modules/company/offers/offers_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:we_work/modules/company/message_for_offer/message_for_offer_screen.dart';
 import 'package:we_work/shared/components/components.dart';
 import 'package:we_work/shared/styles/colors.dart';
